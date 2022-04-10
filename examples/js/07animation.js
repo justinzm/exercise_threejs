518ad5ac47d2e354ada46bc29c1cf6c03fb0c19e
justin郑 <3907721@qq.com>
2022-04-10 08:31:27 +0800

    220408

--- examples/js/07animation.js
+++ examples/js/07animation.js
@@ -10,15 +10,19 @@ let scene, camera, cube, renderer;
 let cameraControl, creeperObj;
 let sphereLightMesh, pointLight;
 let rotateAngle = 0;
-let invert = 1;
+
+let walkSpeed = 0
+let tween, tweenBack
+let invert = 1 // 正反向
+let startTracking = false
 
 // 人物类
-class Creeper{
-  constructor(){
+class Creeper {
+  constructor() {
     // 创建头、身体、脚 几何体
-    const headGeo = new THREE.BoxGeometry(4,4,4);
-    const bodyGeo = new THREE.BoxGeometry(4,8,2);
-    const footGeo = new THREE.BoxGeometry(2,3,2);
+    const headGeo = new THREE.BoxGeometry(4, 4, 4);
+    const bodyGeo = new THREE.BoxGeometry(4, 8, 2);
+    const footGeo = new THREE.BoxGeometry(2, 3, 2);
 
     // 苦力怕脸部贴图
     const headMap = new THREE.TextureLoader().load(
@@ -31,16 +35,16 @@ class Creeper{
 
     // 身体与脚的材质设定
     const skinMat = new THREE.MeshPhongMaterial({
-			map: skinMap // 皮肤贴图
-		})
+      map: skinMap // 皮肤贴图
+    })
 
     // 准备头部与脸的材质
     const headMaterials = [];
-    for(let i = 0; i < 6; i++) {
+    for (let i = 0; i < 6; i++) {
       let map;
-      if(i === 4){
+      if (i === 4) {
         map = headMap;
-      }else{
+      } else {
         map = skinMap;
       }
       headMaterials.push(new THREE.MeshPhongMaterial({ map: map }));
@@ -48,25 +52,25 @@ class Creeper{
 
     // 镜面高光的光泽表面的材质 设为绿色
     // const creeperMat = new THREE.MeshPhongMaterial({color: 0x00ff00});
-    
+
     // 头 建立网络物体
     this.head = new THREE.Mesh(headGeo, headMaterials);
-    this.head.position.set(0,6,0);
+    this.head.position.set(0, 6, 0);
     this.head.rotation.y = 0.5;   // 稍微偏头
 
     // 身体
     this.body = new THREE.Mesh(bodyGeo, skinMat);
-    this.body.position.set(0,0,0);
+    this.body.position.set(0, 0, 0);
 
     // 四只脚
     this.foot1 = new THREE.Mesh(footGeo, skinMat);
-    this.foot1.position.set(-1,-5.5,2);
+    this.foot1.position.set(-1, -5.5, 2);
     this.foot2 = this.foot1.clone();  // 剩下三只脚都复制第一只的 Mesh
-    this.foot2.position.set(-1,-5.5,-2);
+    this.foot2.position.set(-1, -5.5, -2);
     this.foot3 = this.foot1.clone();
-    this.foot3.position.set(1,-5.5,2);
+    this.foot3.position.set(1, -5.5, 2);
     this.foot4 = this.foot1.clone();
-    this.foot4.position.set(1,-5.5,-2);
+    this.foot4.position.set(1, -5.5, -2);
 
     // 将四只脚组合为一个group
     this.feet = new THREE.Group();
@@ -82,8 +86,8 @@ class Creeper{
     this.creeper.add(this.feet);
 
     // 苦力怕投影设置，利用traverse遍历各个子元件设置阴影
-    this.creeper.traverse(function(object){
-      if(object instanceof THREE.Mesh){
+    this.creeper.traverse(function (object) {
+      if (object instanceof THREE.Mesh) {
         object.castShadow = true;     // 投影
         object.receiveShadow = true;  // 接收其他元素投影的效果
       }
@@ -92,12 +96,101 @@ class Creeper{
 }
 
 // 生成苦力怕并加到场景
-function createCreeper(){
+function createCreeper() {
   creeperObj = new Creeper();
+  tweenHandler(); // 追踪相机动画
   scene.add(creeperObj.creeper);
 }
 
-function init(){
+let datGUIControls = new (function () {
+  this.startTracking = false
+})();
+
+function tweenHandler() {
+  // 起始值设定
+  let offset = { x: 0, z: 0, rotateY: 0 }
+  let target = { x: 20, z: 20, rotateY: 0.7853981633974484 } // 目标值
+
+  // 苦力怕走动及转身补间动画
+  const onUpdate = () => {
+    // 移动
+    creeperObj.feet.position.x = offset.x;
+    creeperObj.feet.position.z = offset.z;
+    creeperObj.head.position.x = offset.x;
+    creeperObj.head.position.z = offset.z;
+    creeperObj.body.position.x = offset.x;
+    creeperObj.body.position.z = offset.z;
+
+    // 转身
+    if (target.x > 0) {
+      creeperObj.feet.rotation.y = offset.rotateY
+      creeperObj.head.rotation.y = offset.rotateY
+      creeperObj.body.rotation.y = offset.rotateY
+    } else {
+      creeperObj.feet.rotation.y = -offset.rotateY
+      creeperObj.head.rotation.y = -offset.rotateY
+      creeperObj.body.rotation.y = -offset.rotateY
+    }
+  }
+
+  // 计算新的目标值
+  const handleNewTarget = () => {
+    // 限制苦力怕走路边界
+    if (camera.position.x > 30) {
+      target.x = 20;
+    } else if (camera.position.x < -30) {
+      target.x = -20;
+    } else {
+      target.x = camera.position.x;
+    }
+    if (camera.position.z > 30) {
+      target.z = 20;
+    } else if (camera.position.z < -30) {
+      target.z = -20;
+    } else {
+      target.z = camera.position.z;
+    }
+
+    const v1 = new THREE.Vector2(0, 1); // 原点面向方向
+    const v2 = new THREE.Vector2(target.x, target.z); // 苦力怕面向新相机方向
+
+    // 内积除以标量得两向量 cos 值
+    let cosValue = v1.dot(v2) / (v1.length() * v2.length())
+
+    // 防呆，cos 值区间为（-1, 1）
+    if (cosValue > 1) {
+      cosValue = 1
+    } else if (cosValue < -1) {
+      cosValue = -1
+    }
+
+    // cos 值求转身角度
+    target.rotateY = Math.acos(cosValue);
+  }
+
+  // 朝相机移动
+  tween = new TWEEN.Tween(offset)
+    .to(target, 3000)
+    .easing(TWEEN.Easing.Quadratic.Out)
+    .onUpdate(onUpdate)
+    .onComplete(() => {
+      invert = -1
+      tweenBack.start()
+    });
+
+  // 回原点
+  tweenBack = new TWEEN.Tween(offset)
+    .to({ x: 0, z: 0, rotateY: 0 }, 3000)
+    .easing(TWEEN.Easing.Quadratic.Out)
+    .onUpdate(onUpdate)
+    .onComplete(() => {
+      handleNewTarget() // 计算新的目标值
+      invert = 1
+      tween.start()
+    });
+}
+
+function init() {
   // 01 建立场景
   scene = new THREE.Scene();
 
@@ -112,7 +205,7 @@ function init(){
     0.1,
     1000
   );
-  camera.position.set(30,30,30);  // 相机位置
+  camera.position.set(30, 30, 30);  // 相机位置
   camera.lookAt(scene.position);  // 相机焦点
 
   // 03 三轴座标
@@ -125,7 +218,7 @@ function init(){
   // renderer.setClearColor(0xeeeeee, 1.0);  // 预设背景颜色
   renderer.shadowMap.enable = true;   // 阴影效果
   renderer.shadowMap.type = 2;        // THREE.PCFSoftShadowMap
-  
+
   // 05 建立 OrbitControls
   cameraControl = new THREE.OrbitControls(camera, renderer.domElement);
   cameraControl.enableDamping = true; // 启用阻尼效果
@@ -133,11 +226,11 @@ function init(){
   // cameraControl.autoRotate = true;    // 启用自动旋转
 
   // 06 建立地板
-  const planeGeometry = new THREE.PlaneGeometry(60,60); // 生成平面几何体的类。
-  const planeMaterial = new THREE.MeshLambertMaterial({color: 0xffffff}); // 一种非光泽表面的材质，没有镜面高光
+  const planeGeometry = new THREE.PlaneGeometry(60, 60); // 生成平面几何体的类。
+  const planeMaterial = new THREE.MeshLambertMaterial({ color: 0xffffff }); // 一种非光泽表面的材质，没有镜面高光
   let plane = new THREE.Mesh(planeGeometry, planeMaterial);
   plane.rotation.x = -0.5 * Math.PI;  // 使平面与y轴垂直，并让正面朝上
-  plane.position.set(0,-7,0);
+  plane.position.set(0, -7, 0);
   plane.receiveShadow = true;   // 接收其他元素投影的效果
   scene.add(plane);
 
@@ -156,7 +249,7 @@ function init(){
 
   // 设置聚光灯 照亮物体
   let spotLight = new THREE.SpotLight(0xf0f0f0);
-  spotLight.position.set(-10,30,20);
+  spotLight.position.set(-10, 30, 20);
   // spotLight.castShadow = true;
   scene.add(spotLight);
 
@@ -167,11 +260,30 @@ function init(){
 
   // 小球体模拟点光源实体
   const sphereLightGeo = new THREE.SphereGeometry(0.3);
-  const sphereLightMat = new THREE.MeshBasicMaterial({color: 0xccffcc});
+  const sphereLightMat = new THREE.MeshBasicMaterial({ color: 0xccffcc });
   sphereLightMesh = new THREE.Mesh(sphereLightGeo, sphereLightMat);
   sphereLightMesh.castShadow = true;
   sphereLightMesh.position.y = 16;
-  scene.add(sphereLightMesh)
+  scene.add(sphereLightMesh);
+
+  // dat.GUI 控制面板
+  gui = new dat.GUI()
+  gui.add(datGUIControls, 'startTracking').onChange(function (e) {
+    startTracking = e
+    if (invert > 0) {
+      if (startTracking) {
+        tween.start()
+      } else {
+        tween.stop()
+      }
+    } else {
+      if (startTracking) {
+        tweenBack.start()
+      } else {
+        tweenBack.stop()
+      }
+    }
+  });
 
   // 将渲染器的DOM绑到网页上
   document.body.appendChild(renderer.domElement);
@@ -179,28 +291,28 @@ function init(){
 
 // 点光源绕 Y 轴旋转动画
 function pointLightAnimation() {
-	if (rotateAngle > 2 * Math.PI) {
-		rotateAngle = 0; // 超过 360 度后归零
-	} else {
-		rotateAngle += 0.03; // 递增角度
-	}
-	// 光源延椭圆轨道绕 Y 轴旋转
-	sphereLightMesh.position.x = 8 * Math.cos(rotateAngle);
-	sphereLightMesh.position.z = 4 * Math.sin(rotateAngle);
-	// 点光源位置与球体同步
-	pointLight.position.copy(sphereLightMesh.position);
+  if (rotateAngle > 2 * Math.PI) {
+    rotateAngle = 0; // 超过 360 度后归零
+  } else {
+    rotateAngle += 0.03; // 递增角度
+  }
+  // 光源延椭圆轨道绕 Y 轴旋转
+  sphereLightMesh.position.x = 8 * Math.cos(rotateAngle);
+  sphereLightMesh.position.z = 4 * Math.sin(rotateAngle);
+  // 点光源位置与球体同步
+  pointLight.position.copy(sphereLightMesh.position);
 }
 
 let rotateheadOffset = 0;
 // 摆头
-function creeperHeadRotate(){
+function creeperHeadRotate() {
   rotateheadOffset += 0.04;
   creeperObj.head.rotation.y = Math.sin(rotateheadOffset);
 }
 
 let walkOffset = 0;
 // 走动
-function creeperFeetWalk(){
+function creeperFeetWalk() {
   walkOffset += 0.04;
   creeperObj.foot1.rotation.x = Math.sin(walkOffset) / 4;
   creeperObj.foot2.rotation.x = -Math.sin(walkOffset) / 4;
@@ -210,18 +322,18 @@ function creeperFeetWalk(){
 
 let scaleHeadOffset = 0
 // 膨胀
-function creeperScaleBoby(){
+function creeperScaleBoby() {
   scaleHeadOffset += 0.04;
   let scaleRate = Math.abs(Math.sin(scaleHeadOffset)) / 16 + 1;
   creeperObj.creeper.scale.set(scaleRate, scaleRate, scaleRate);
 }
 
 // 渲染场景
-function render(){
+function render() {
   creeperHeadRotate();
   creeperFeetWalk();
   creeperScaleBoby();
-
+  TWEEN.update(); // update
   cameraControl.update();
   pointLightAnimation();
   requestAnimationFrame(render);
@@ -229,7 +341,7 @@ function render(){
 }
 
 // 监听屏幕宽高变化来做简单 RWD 设定
-window.addEventListener('resize', function() {
+window.addEventListener('resize', function () {
   camera.aspect = window.innerWidth / window.innerHeight
   camera.updateProjectionMatrix()
   renderer.setSize(window.innerWidth, window.innerHeight)
